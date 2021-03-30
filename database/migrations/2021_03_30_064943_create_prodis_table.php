b383bd9bee9e95247cd2ddee7dd99b137007760e
RedhoSyahkumar <syahkumarr@gmail.com>
2021-03-30 18:02:04 +0700

    tugas_migration

---
+++ database/migrations/2021_03_30_064943_create_prodis_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prodis', function (Blueprint $table) {
+            $table->id();
+            $table->char('nidn', 10);
+            $table->string('nama', 50);
+            $table->char('kode_prodi', 5);
+            $table->timestamps();
+
+            $table->index('kode_prodi');
+
+        
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prodis');
+    }
+}
